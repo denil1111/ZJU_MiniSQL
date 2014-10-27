7a60f356e2c2c441d1d356cfd1e2065fe1ef4978
denil1111 <zyh.work@gmail.com>
2014-10-27 13:28:48 +0800

    New Xcode project

---
+++ MiniSQL/MiniSQL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MiniSQL
+//
+//  Created by zyhc on 10/27/14.
+//  Copyright (c) 2014 zyhc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
